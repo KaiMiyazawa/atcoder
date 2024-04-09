9ec7fb702bff79d9025d109feed873f418537f2c
roge_mega <124577165+KaiMiyazawa@users.noreply.github.com>
2024-04-10 01:55:54 +0900

    Update A.py

--- ABC342/A.py
+++ ABC342/A.py
@@ -1,1 +1,11 @@
+s = input()
 
+if (s[0] != s[1]):
+  if (s[0] != s[2]):
+    print("1")
+  if (s[0] == s[2]):
+    print("2")
+else:
+  for i in range(len(s)):
+    if (s[0] != s[i]):
+      print(i+1)
